bec59f9c643eba0a487181d37e2cbb6c0af9ffb7
Timofey Martynov <mart-tima@yandex.ru>
2020-10-12 18:02:13 +0500

    Add Spawner

---
+++ Assets/Scripts/Core/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private float BombPerMinute = 30;
+    private float FoodPerMinute = 20;
+
+
+    private void Start()
+    {
+        Invoke("StartSpawns", 1f);
+    }
+
+
+    private IEnumerator SpawnFood()
+    {
+        while (true)
+        {
+            ObjectPooler.Instance.SpawnFromPool("Food", Random.onUnitSphere * 15f, Quaternion.identity);
+            yield return new WaitForSeconds(60f / FoodPerMinute);
+        }
+    }
+
+
+    IEnumerator SpawnBomb()
+    {
+        yield break;
+    }
+
+
+    private void StartSpawns()
+    {
+        StartCoroutine("SpawnFood");
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            ObjectPooler.Instance.SpawnFromPool("Food", Random.onUnitSphere * 15f, Quaternion.identity);
+        }
+    }
+}
