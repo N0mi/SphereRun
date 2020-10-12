ef4d75c5fea0796414c80f0f2c32179c458c602a
Timofey Martynov <mart-tima@yandex.ru>
2020-10-12 12:42:02 +0500

    Create player controll + prefabs

---
+++ Assets/Scripts/LookAtScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtScript : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.LookAt(Vector3.zero);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 1f;
+    [SerializeField]
+    private float RotateSpeed = 1f;
+
+    private Rigidbody rb;
+    private Vector3 MoveDirection;
+    private float horizontal;
+
+    private readonly float gravity = -50;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        MoveDirection = new Vector3(0, 0, Input.GetAxisRaw("Vertical"));
+        horizontal = Input.GetAxisRaw("Horizontal") * RotateSpeed;
+        //if(Input.GetButtonDown("Jump"))
+        //{
+        //    Instantiate(Resources.Load("Prefabs/Eat"), Random.onUnitSphere * 15f, Quaternion.identity);
+        //}
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 gravityUp = (transform.position - Vector3.zero).normalized;
+
+        Quaternion targetRotation = Quaternion.FromToRotation(transform.up, gravityUp) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 50f * Time.deltaTime);
+        transform.rotation = transform.rotation * Quaternion.AngleAxis(horizontal, Vector3.up);
+
+        rb.AddForce(gravityUp * gravity);
+        rb.MovePosition(rb.position + transform.TransformDirection(MoveDirection.normalized) * Speed / 10f);
+
+        //Vector3 origin = Vector3.zero;
+
+        //Quaternion hq = ;
+        //Quaternion vq = Quaternion.AngleAxis(verical, Vector3.right);
+
+        //Quaternion q = hq * vq;
+
+        //rb.MovePosition(q * (rb.transform.position - origin) + origin);
+        //transform.LookAt(Vector3.zero, transform.up);
+    }
+}
