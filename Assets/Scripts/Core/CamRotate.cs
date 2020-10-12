68b6c955d481947a1494a1fdb3097118c43d7ce9
Timofey Martynov <mart-tima@yandex.ru>
2020-10-12 14:05:12 +0500

    Add rotate cam

---
+++ Assets/Scripts/Core/CamRotate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CamRotate : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Camera cam;
+
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Start");
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 delta = eventData.delta / 10f;
+
+        cam.transform.position = new Vector3();
+
+        cam.transform.Rotate(Vector3.right, -delta.y);
+        cam.transform.Rotate(Vector3.up, delta.x);
+        cam.transform.Translate(new Vector3(0, 0, -120));
+    }
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End");
+    }
+}
