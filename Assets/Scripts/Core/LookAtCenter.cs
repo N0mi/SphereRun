862b50c41476be277aa042f49af75f529e691d99
Timofey Martynov <mart-tima@yandex.ru>
2020-10-13 15:21:18 +0500

    update rotate object on planet

---
+++ Assets/Scripts/Core/LookAtCenter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCenter : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 gravityUp = (transform.position - Vector3.zero).normalized;
+        Quaternion targetRotation = Quaternion.FromToRotation(transform.up, gravityUp) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 50f * Time.deltaTime);
+    }
+}
